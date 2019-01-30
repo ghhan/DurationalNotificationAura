2bfb39abc4419c4f1cc2af089207174f64a0981a
안단태 <48adt@naver.com>
2019-01-30 15:15:40 +0900

    UnityPlayerActivity

---
+++ app/src/main/java/kr/ac/snu/hcil/durationalnotificationaura/UnityEffect.kt
@@ -0,0 +1,21 @@
+package kr.ac.snu.hcil.durationalnotificationaura
+
+import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.snu.hcil.durationalnotificationaura.ui.enhancedhomescreen.EnhancedHomeScreenViewModel
+import kr.ac.snu.hcil.visualeffecttest.UnityPlayerActivity
+
+class UnityEffect : AppCompatActivity() {
+
+    private lateinit var viewModel: EnhancedHomeScreenViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_unity_effect)
+        viewModel = ViewModelProviders.of(this).get(EnhancedHomeScreenViewModel::class.java)
+
+        val intent = Intent(this, UnityPlayerActivity::class.java)
+        startActivity(intent)
+    }
+}
